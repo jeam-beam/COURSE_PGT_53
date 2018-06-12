8ed81cdcb519045f3376f397a16c7c51bd39608d
jeam-beam <jeam3n@gmail.com>
2018-06-12 20:00:28 +0500

    test1

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testPoint() {
+//    Point p1 = new Point(5, 6);
+//    Assert.assertEquals(p1.distance(),25.0);
+  }
+}
