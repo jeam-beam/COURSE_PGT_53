f5fde211dffcaddd580dd93ca7b1a2fc787aba9f
jeam-beam <jeam3n@gmail.com>
2018-06-12 17:49:39 +0500

    Расстояние между точками (исправлены каталоги)

--- sandbox/src/main/java/zadanie_2/Main.java
+++ sandbox/src/main/java/zadanie_2/Main.java
@@ -1,30 +1,26 @@
-package ru.stqa.pft.sandbox;
+package zadanie_2;
 
-public class Point {
-  public double x;
-  public double y;
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
-
-
+public class Main {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(1,1);
+    Point p1 = new Point(1,2);
     System.out.println("Есть точка P1 с координатами x " + p1.x + " и y " + p1.y );
 
     Point p2 = new Point(5,3);
     System.out.println("Есть точка P2 с координатами x " + p2.x + " и y " + p2.y );
 
     System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + distance(p1, p2));
+//    System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + p1.distance());
 
   }
 
 
-  public static double distance(Point p1, Point p2) {
+    public static double distance(Point p1, Point p2) {
     return Math.sqrt((Math.pow(p2.x - p1.x, 2)) + (Math.pow(p2.y - p1.y, 2)));
   }
+//  public double distance() {
+//    return Math.sqrt((Math.pow(p2.x - p1.x, 2)) + (Math.pow(p2.y - p1.y, 2)));
+//  }
 }
 
