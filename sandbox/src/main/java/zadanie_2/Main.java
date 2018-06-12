611ba8d112635d1b25e8018be5a65a1ef191307c
jeam-beam <jeam3n@gmail.com>
2018-06-12 18:11:38 +0500

    Добавлен метод в класс Point

--- sandbox/src/main/java/zadanie_2/Main.java
+++ sandbox/src/main/java/zadanie_2/Main.java
@@ -10,8 +10,8 @@ public class Main {
     Point p2 = new Point(5,3);
     System.out.println("Есть точка P2 с координатами x " + p2.x + " и y " + p2.y );
 
-    System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + distance(p1, p2));
-//    System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + p1.distance());
+    System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + distance(p1, p2) + "   --Функция");
+    System.out.println("Расстояние между точками P1 и P2 на плоскоти = " + p2.distance(p1) + "   --метод");
 
   }
 
@@ -19,8 +19,6 @@ public class Main {
     public static double distance(Point p1, Point p2) {
     return Math.sqrt((Math.pow(p2.x - p1.x, 2)) + (Math.pow(p2.y - p1.y, 2)));
   }
-//  public double distance() {
-//    return Math.sqrt((Math.pow(p2.x - p1.x, 2)) + (Math.pow(p2.y - p1.y, 2)));
-//  }
+
 }
 
--- sandbox/src/main/java/zadanie_2/Point.java
+++ sandbox/src/main/java/zadanie_2/Point.java
@@ -8,4 +8,8 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point p1) {
+    return Math.sqrt((Math.pow(this.x - p1.x, 2)) + (Math.pow(this.y - p1.y, 2)));
+  }
 }
