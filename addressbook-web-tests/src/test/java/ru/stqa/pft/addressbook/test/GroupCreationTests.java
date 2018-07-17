ebaa68700361e81c0a8beb66f1c5e95ad9c2c0b8
jeam-beam <jeam3n@gmail.com>
2018-07-17 22:28:45 +0500

    5.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -1,31 +1,24 @@
 package ru.stqa.pft.addressbook.test;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().groupPage();
-    //List<GroupData> before = app.group().list();
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
-    //List<GroupData> after = app.group().list();
-    Set<GroupData> after = app.group().all();
+    Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
 
-    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    before.add(group);
-    assertThat(after, equalTo(before.withAdded(group)));
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
